6cf94e38fd2f368ffd271ca96a76c36b8a1856e9
temka5228 <70948892+temka5228@users.noreply.github.com>
2020-09-20 21:37:36 +0500

    Добавил расшифровку

--- cezar_.py
+++ cezar_.py
@@ -2,12 +2,14 @@ LittleLetters: str = 'абвгдеёжзийклмнопрстуфхцчшщъы
 BigLetters: str = 'AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 while True:
 	NewCoddedMessage, Message = '', ''
-	check = input('Вы хотите закодировать сообщение?, 1 - да, 2 - нет: ')
+	check = input('Вы хотите совершить операцию над сообщением?, 1 - да, 2 -нет : ')
 	if check == '2':
 		break
 	elif check == '1':
-		Message = input('Введите сообщение, которое хотите закодировать: ')
+		check2 = input('Вы хотите раскодировать или закодировать сообщение?, 1 - раскодировать, 2 - закодировать : ')
+		Message = input('Введите сообщение: ')
 		Shift = int(input('Насколько хотите сдвинуть буквы в строке?: '))
+		if check2 == '1': Shift = (-1)*Shift
 		for letter in range(len(Message)):
 			if Message[letter] in LittleLetters:
 				NumberInAlphabet = LittleLetters.index(Message[letter])
