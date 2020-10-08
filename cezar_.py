fd57536f5587e7c3fef97e6749c5149fb9ef39ce
temka5228 <70948892+temka5228@users.noreply.github.com>
2020-10-08 20:08:42 +0500

    commets

--- cezar_.py
+++ cezar_.py
@@ -1,30 +1,30 @@
 LittleLetters: str = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 BigLetters: str = 'AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-while True:
-	NewCoddedMessage, Message = '', ''
-	check = input('Вы хотите совершить операцию над сообщением?, 1 - да, 2 -нет : ')
-	if check == '2':
-		break
-	elif check == '1':
-		check2 = input('Вы хотите раскодировать или закодировать сообщение?, 1 - раскодировать, 2 - закодировать : ')
-		Message = input('Введите сообщение: ')
-		Shift = int(input('Насколько хотите сдвинуть буквы в строке?: '))
-		if check2 == '1': Shift = (-1)*Shift
-		for letter in range(len(Message)):
-			if Message[letter] in LittleLetters:
-				NumberInAlphabet = LittleLetters.index(Message[letter])
-				if 32 - NumberInAlphabet < Shift:
-					delta = - 33 + Shift
-				else:
-					delta = Shift
-				NewCoddedMessage += LittleLetters[NumberInAlphabet + delta]
-			elif Message[letter] in BigLetters:
-				NumberInAlphabet = BigLetters.index(Message[letter])
-				if 32 - NumberInAlphabet < Shift:
-					delta = - 33 + Shift
-				else:
-					delta = Shift
-				NewCoddedMessage += BigLetters[NumberInAlphabet + delta]
-			else:
-				NewCoddedMessage += Message[letter]
-		print(NewCoddedMessage)
+while True:  # Вообще если при выборе кодировки или раскодировки или хотим ли мы выйти из программы. при вводе других
+    NewCoddedMessage, Message = '', ''  # символов цикл просто начнеться заново - так сказать проверочка.
+    check = input('Вы хотите совершить операцию над сообщением?, 1 - да, 2 -нет : ')
+    if check == '2':  # Проверка на выход.
+        break
+    elif check == '1':
+        check2 = input('Вы хотите раскодировать или закодировать сообщение?, 1 - раскодировать, 2 - закодировать : ')
+        Message = input('Введите сообщение: ')
+        Shift = int(input('Введите смещение: '))
+        if check2 == '1': Shift = (-1) * Shift  # Если сообщение надо раскодить,
+        for letter in range(len(Message)):  # то смещение делаем в обратную сторону.
+            if Message[letter] in LittleLetters:  # Если буква в нижнем регистре.
+                NumberInAlphabet = LittleLetters.index(Message[letter])  # Узнаем какая по счету буква в сообщении.
+                if 32 - NumberInAlphabet < Shift:  # Проверка: если прибавить смещение к номеру буквы в сообщении
+                    delta = - 33 + Shift  # выходит ли за рамки длинны алфавита. Если выходит -
+                else:  # то остаток начинаем с начала алфавита.
+                    delta = Shift
+                NewCoddedMessage += LittleLetters[NumberInAlphabet + delta]  # Добавляем букву в новое сообщение
+            elif Message[letter] in BigLetters:  # Если буква в верхнем регистре. По факту то же самое,
+                NumberInAlphabet = BigLetters.index(Message[letter])  # что и с нижним регистром.
+                if 32 - NumberInAlphabet < Shift:
+                    delta = - 33 + Shift
+                else:
+                    delta = Shift
+                NewCoddedMessage += BigLetters[NumberInAlphabet + delta]
+            else:
+                NewCoddedMessage += Message[letter]
+        print('Полученное сообщение:', NewCoddedMessage)
