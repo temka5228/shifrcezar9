65632538369a0b654bae035d6a492cadb58f71f2
temka5228 <70948892+temka5228@users.noreply.github.com>
2020-10-25 18:22:51 +0500

    Добавлены логи

--- cezar_.py
+++ cezar_.py
@@ -1,15 +1,38 @@
+import logging
+
+logger = logging.getLogger('First logger')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('cezar.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.info('Program started')
+
+
 LittleLetters: str = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 BigLetters: str = 'AБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 while True:  # Вообще если при выборе кодировки или раскодировки или хотим ли мы выйти из программы. при вводе других
     NewCoddedMessage, Message = '', ''  # символов цикл просто начнеться заново - так сказать проверочка.
     check = input('Вы хотите совершить операцию над сообщением?, 1 - да, 2 -нет : ')
     if check == '2':  # Проверка на выход.
+        logger.info('Program stopped')
         break
     elif check == '1':
-        check2 = input('Вы хотите раскодировать или закодировать сообщение?, 1 - раскодировать, 2 - закодировать : ')
         Message = input('Введите сообщение: ')
+        logger.info('Inputted message: %s' % Message)
         Shift = int(input('Введите смещение: '))
-        if check2 == '1': Shift = (-1) * Shift  # Если сообщение надо раскодить,
+        logger.info('Inputted shift: %s' % Shift)
+        check2 = input('Вы хотите раскодировать или закодировать сообщение?, 1 - раскодировать, 2 - закодировать : ')
+        if check2 == '1':
+            logger.info('type of program operation: decryption')
+            Shift = (-1) * Shift
+        elif check2 == '2':
+            logger.info('type of program operation: encryption')
+        else:
+            logger.info('Incorrect input')
+            continue
+
+        logger.info('Program started working on the message')
         for letter in range(len(Message)):  # то смещение делаем в обратную сторону.
             if Message[letter] in LittleLetters:  # Если буква в нижнем регистре.
                 NumberInAlphabet = LittleLetters.index(Message[letter])  # Узнаем какая по счету буква в сообщении.
@@ -27,4 +50,5 @@ while True:  # Вообще если при выборе кодировки ил
                 NewCoddedMessage += BigLetters[NumberInAlphabet + delta]
             else:
                 NewCoddedMessage += Message[letter]
+        logger.info('Program successfully finished working on the message')
         print('Полученное сообщение:', NewCoddedMessage)
